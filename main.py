80afeca8195a2a154fbce796347106bb839ff8e1
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-04-04 22:12:11 +0900

    리드미 블로그 게시글 자동화

---
+++ main.py
@@ -0,0 +1,26 @@
+import feedparser, time
+
+URL = "https://mjrecord.tistory.com/rss"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST = 5
+
+markdown_text = """
+### Hi there 👋
+My name is **DongYoung**, major in **computer science**💻 since 2017 at **Pusan National University** in Pusan, South Korea.  
+**Front-end developer**, write code with **react & typescript**.
+
+📂 Blog | [https://ehddud100677.tistory.com/](https://ehddud100677.tistory.com/)
+
+### My latest posts
+"""  # list of blog posts will be appended here
+
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        feed_date = feed['published_parsed']
+        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+        
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
